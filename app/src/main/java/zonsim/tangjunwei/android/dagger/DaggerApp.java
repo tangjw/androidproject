895a39452e262fddc65c39d567f459be9a166d45
tangjw <tjwabc@gmail.com>
2018-03-08 18:19:50 +0800

    dagger

---
+++ app/src/main/java/zonsim/tangjunwei/android/dagger/DaggerApp.java
@@ -0,0 +1,20 @@
+package zonsim.tangjunwei.android.dagger;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+
+/**
+ * desc
+ *
+ * @author tangjunwei
+ * @email tjwabc@gmail.com
+ * Created by company-pc on 2018/3/8.
+ */
+
+@SuppressLint("Registered")
+public class DaggerApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/zonsim/tangjunwei/android/dagger/activity/SplashActivity.java
@@ -0,0 +1,25 @@
+package zonsim.tangjunwei.android.dagger.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * desc
+ *
+ * @author tangjunwei
+ * @email tjwabc@gmail.com
+ * Created by company-pc on 2018/3/8.
+ */
+
+public class SplashActivity extends BaseActivity {
+    
+    @Override
+    protected void setupComponent() {
+    
+    }
+    
+    @Override
+    protected void setupCreate(@Nullable Bundle savedInstanceState) {
+    
+    }
+}
